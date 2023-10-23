fa6df42c0848077a1370013fd0140f9786c1cff6
Cleber-esx <cleber.batista@esx.com.br>
2023-10-23 18:11:42 -0300

    Adicionando increment

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,18 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int totalClicks = 0;
+
   @override
   Widget build(BuildContext context) {
+    Text text1 = Text('exemplo 55');
+    Text text2 = Text('exemplo 55');
+    TextField entrada = TextField();
+
     return MaterialApp(
       title: 'Meu app',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Cleber Batista Dev '),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
         ),
         body: Center(
-          child: Text('Seja bem vindo'),
-        ),
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Total de clicks  ${totalClicks}",
+              style: TextStyle(color: Colors.black),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Clicou');
+                setState(() {
+                  totalClicks++;
+                });
+              },
+              child: Text('Clicar'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.greenAccent),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
