80dec806b2bb52de472a8f627da84a4e9840432a
Cleber-esx <cleber.batista@esx.com.br>
2023-10-26 14:49:19 -0300

    Finalizando projeto calculadora de soma em flutter

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,53 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Calculator());
 
-class MyApp extends StatefulWidget {
+class Calculator extends StatelessWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Calculadora de soma'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: CalculatorScreen(),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  int totalClicks = 0;
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String result = ' ';
+  double number1 = 0;
+  double number2 = 0;
 
   @override
   Widget build(BuildContext context) {
-    Text text1 = Text('exemplo 55');
-    Text text2 = Text('exemplo 55');
-    TextField entrada = TextField();
-
-    return MaterialApp(
-      title: 'Meu app',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cleber Batista Dev '),
-          centerTitle: true,
-          backgroundColor: Colors.greenAccent,
+    TextField num1 = TextField(
+        keyboardType: TextInputType.number,
+        style: TextStyle(color: Colors.red),
+        decoration: InputDecoration(
+          labelText: 'Número 01 ',
+          labelStyle: TextStyle(color: Colors.red),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black87, width: 2.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black87, width: 2.0),
+          ),
         ),
-        body: Center(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Total de clicks  ${totalClicks}",
-              style: TextStyle(color: Colors.black),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('Clicou');
-                setState(() {
-                  totalClicks++;
-                });
-              },
-              child: Text('Clicar'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.greenAccent),
-              ),
-            )
-          ],
-        )),
+        onChanged: (value) {
+          number1 = double.parse(value);
+        });
+
+    TextField num2 = TextField(
+        keyboardType: TextInputType.number,
+        style: TextStyle(color: Colors.red),
+        decoration: InputDecoration(
+          labelText: 'Número 02 ',
+          labelStyle: TextStyle(color: Colors.red),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black87, width: 2.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black87, width: 2.0),
+          ),
+        ),
+        onChanged: (value) {
+          number2 = double.parse(value);
+        });
+
+    ElevatedButton botao = ElevatedButton(
+      onPressed: () {
+        setState(() {
+          double calc = number1 + number2;
+          this.result = " O resultado da soma é ${calc.toStringAsFixed(0)}";
+        });
+      },
+      child: Text('Calcular'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.red,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
       ),
+    );
+
+    Text result = Text(
+      this.result,
+      style: TextStyle(
+        color: Colors.red,
+        fontSize: 20,
+      ),
+    );
+
+    Padding separator = Padding(padding: EdgeInsets.all(5.0));
+
+    return Column(
+      children: <Widget>[
+        num1,
+        separator,
+        num2,
+        separator,
+        SizedBox(
+          child: botao,
+          width: double.infinity,
+        ),
+        separator,
+        result
+      ],
     );
   }
 }
